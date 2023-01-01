fe8286dff1396897b561f7c426ee1e623979b2ea
Alireza <alirezashadow52555@gmail.com>
2023-01-01 22:13:37 +0330

    react update

--- front-end/dex/blockchain/contracts/InvesweetDex.sol
+++ front-end/dex/blockchain/contracts/InvesweetDex.sol
@@ -9,6 +9,9 @@ contract PairOfDex{
 
 mapping (address => mapping(uint256 => uint256)) public orderOn1;
 mapping (address => uint256) public orderOn2;
+mapping (uint256 => mapping(uint256 => address)) public ordersInThi;
+
+// mapping (uint256 => mapping(address =>uint256))
 
 IERC20 public token1 ;
 IERC20 public token2 ;
@@ -18,13 +21,55 @@ IERC20 public token2 ;
         token1 = IERC20(secondTokenAddress);
     }
 
+    struct inThisPrice {
+        mapping (uint256 => address) amountToAddress;
+        uint256 Amount;
+        address userAddres;
+        uint256[] orderAmounts;
+        mapping (uint256 => address) orderToUser;
+    }
+    inThisPrice[] inEachPrice;
+    mapping (uint256 => uint256) public orderInThisPrice;
+    mapping (uint256 => inThisPrice) public getAmountFrom;
+
     function orderBuy (IERC20 tokenIn, uint256 amount, uint256 price) external{
 
         bool isToken1 = tokenIn ==  token1;
         (IERC20 tokenIn, IERC20 tokenOut) = isToken1 ? (token1, token2) : (token1, token2) ;
 
+        address sender;
+
         tokenIn.approve(address(this),amount);
         orderOn1[msg.sender][price] = amount;
+        if (orderInThisPrice[price] > amount) {
+                if (getAmountFrom[price].orderAmounts[0] > amount) {
+
+                    uint256 indexAmount = getAmountFrom[price].orderAmounts[0];
+
+                    sender = getAmountFrom[price].orderToUser[indexAmount];
+                    tokenOut.transferFrom(sender,msg.sender,amount);
+                } else {
+                    for (uint i = 0; i < getAmountFrom[price].orderAmounts.length; i++) {
+
+                        }
+                }
+            
+            // tokenOut.transferFrom()
+        }
+    }
+
+    uint256 [] arr;
+    function remove(uint _index) public {
+        require(_index < arr.length, "index out of bound");
+
+        for (uint i = _index; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr.pop();
+    }
+
+    function matchOrder () internal {
+
     }
 
 }
